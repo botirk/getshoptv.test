6c52f944769ad87671842810fb4b030152aad7f9
Ivan <antipchel@gmail.com>
2021-07-30 19:56:30 +0300

    +phone number alpha version

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import '../css/index.css';
-import React from 'react';
+import React, { useState } from 'react';
 
 // functional components
 const Container = ({ children }) => {
@@ -9,8 +9,21 @@ const Container = ({ children }) => {
   </div>;
 }
 
+const Phone = ({ phone }) => {
+  return <p className="mt-13 fs-32 text-center"><b>+7{phone}</b></p>;
+}
+
+const Menu = () => {
+  const [phone, setPhone] = useState('');
+
+  return <div className="menu">
+    <p className="mt-72 fs-26 text-center">Введите ваш номер мобильного телефона</p>
+    <Phone phone={"915-55-44-33"} />
+  </div>
+}
+
 export default () => {
   return <Container>
-
+    <Menu/>
   </Container>;
 }
